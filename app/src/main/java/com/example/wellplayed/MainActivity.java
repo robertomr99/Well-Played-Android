6543d9dc9efb1a1619253eb81809337b26ac82ea
Miguel Ángel Rey <miguelangelreyalvarez@gmail.com>
2021-12-22 20:11:51 +0100

    Creación del menú hamburguesa

--- app/src/main/java/com/example/wellplayed/MainActivity.java
+++ app/src/main/java/com/example/wellplayed/MainActivity.java
@@ -1,17 +1,70 @@
 package com.example.wellplayed;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
-    Button btn1;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    DrawerLayout drawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,0,0);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+        openFragment(new initFragment());
     }
+    private void openFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        switch (item.getItemId()){
+            case R.id.mnuPerfil: openFragment(new perfilFragment());
+                break;
+            case R.id.mnuMisJuegos: openFragment(new misJuegosFragment());
+                break;
+            case R.id.mnuMisEquipos: openFragment(new misEquiposFragment());
+                break;
+            case R.id.mnuMisPartidos: openFragment(new misPartidosFragment());
+                break;
+            case R.id.mnuSoporte: openFragment(new soporteFragment());
+                break;
+            case R.id.mnuAjustes: openFragment(new ajustesFragment());
+                break;
+
+        }
+        return false;
+    }
+
+
 }
