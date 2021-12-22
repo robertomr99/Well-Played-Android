83610a6d767bc7389d5890a391412db6b4131499
Miguel Ángel Rey <miguelangelreyalvarez@gmail.com>
2021-12-22 17:04:26 +0100

    Primeras variables

--- app/src/main/java/com/example/wellplayed/MainActivity.java
+++ app/src/main/java/com/example/wellplayed/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.wellplayed;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
