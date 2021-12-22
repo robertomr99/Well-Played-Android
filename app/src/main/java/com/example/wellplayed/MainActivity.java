1d3c518c81dc8a59b60138a1b81376c0ecbf1733
Miguel <alacidmiguel@gmail.com>
2021-12-22 20:09:30 +0100

    Creación de los layouts; Login y registro 22/12/2021

--- app/src/main/java/com/example/wellplayed/MainActivity.java
+++ app/src/main/java/com/example/wellplayed/MainActivity.java
@@ -2,21 +2,24 @@ package com.example.wellplayed;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText prueba;
-
-    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.btnLogin).setOnClickListener(view -> {
 
+            Intent i = new Intent(this,Login.class);
+            startActivity(i);
+
+        });
     }
 }
