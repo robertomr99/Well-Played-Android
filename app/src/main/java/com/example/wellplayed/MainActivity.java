23e632c3ce09ff7c90d60681752cf11a69ae8d94
Miguel <alacidmiguel@gmail.com>
2021-12-23 17:11:45 +0100

    ventana Login terminando

--- app/src/main/java/com/example/wellplayed/MainActivity.java
+++ app/src/main/java/com/example/wellplayed/MainActivity.java
@@ -94,7 +94,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     }
 
-    @Override public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if(keyCode==KeyEvent.KEYCODE_BACK) { return false; } return super.onKeyDown(keyCode, event);
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
     }
 }
