dc8ed04eda4b20ff96e23023ffcb59806fc60c75
Miguel <alacidmiguel@gmail.com>
2022-01-17 14:42:38 +0100

    Cambiar logo de equipo terminado 17/01/2022

--- app/src/main/java/com/example/wellplayed/crearEquipo.java
+++ app/src/main/java/com/example/wellplayed/crearEquipo.java
@@ -10,7 +10,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -28,6 +30,8 @@ import java.util.List;
 public class crearEquipo extends AppCompatActivity {
 
     Producto oProducto;
+    TextView lblNombreEquipoCrear;
+    CheckBox chechBoxLOL;
     public static ImageView imgViewEquipo;
 
     @Override
@@ -35,51 +39,88 @@ public class crearEquipo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_equipo);
         imgViewEquipo = findViewById(R.id.imgViewEquipo);
-
-        cambiarLogo();
+        lblNombreEquipoCrear = findViewById(R.id.lblNombreEquipoCrear);
+        chechBoxLOL = findViewById(R.id.checkBoxLOL);
 
         cancelar();
     }
 
-    public void cancelar(){
+    public void cancelar() {
 
         // Para volver de una actividad a la ventana anterior.
 
-        findViewById(R.id.btnCancelar).setOnClickListener( v ->{
+        findViewById(R.id.btnCancelar).setOnClickListener(v -> {
             super.onBackPressed();
         });
     }
 
-    public void cambiarLogo() {
-
-        imgViewEquipo.setOnClickListener(v -> {
-            Intent intentLogin = new Intent(this, Logos.class);
-            startActivityForResult(intentLogin, 1);
-        });
+    public void cambiarLogo(View view) {
+        Intent intentLogin = new Intent(this, Logos.class);
+        startActivityForResult(intentLogin, 1);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == 1) {
-
+        if (requestCode != RESULT_CANCELED) {
             if (resultCode == Activity.RESULT_OK) {
-                intentProducto();
+                oProducto = (Producto) data.getExtras().get("producto"); // el parametro intent data lo que hace es
+                Glide.with(getApplicationContext()).load(oProducto.getsFoto()).circleCrop().into(imgViewEquipo);
+
+
             }
-            if (resultCode == Activity.RESULT_CANCELED) {
-                Toast.makeText(this, "No se ha podido añadir el avatar seleccionado", Toast.LENGTH_SHORT).show();
-            }
+        } else {
+            Toast.makeText(this, "No se ha podido añadir el avatar seleccionado", Toast.LENGTH_SHORT).show();
+
         }
     } //onActivityResult
 
-    private void intentProducto() {
-        try {
-            if (getIntent().hasExtra("producto")) {
-                oProducto = (Producto) getIntent().getSerializableExtra("producto");
+    private void insertEquipo() {
+
+            String sUrl = Utils.hosting + "equipo/ins-equipo.php?txtNombre="+lblNombreEquipoCrear.getText().toString()+"&txtFoto="+oProducto.getsFoto();
+
+            Volley.newRequestQueue(this).add(new StringRequest(Request.Method.GET,sUrl,
+                    s ->{
+                        if(s.equals("null")){
+                            Toast.makeText(getApplicationContext(), "error al crear el equipo", Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Equipo creado con éxito", Toast.LENGTH_LONG).show();
+                            selectiIdEquipo();
+                            insertEquipoJuego();
+                        }
+                    }
+                    ,volleyError -> {
+
+                Log.d("ALACID",volleyError.getCause().toString());
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            ));
+
     }
+
+    private void insertEquipoJuego() {
+        int iIdJuego;
+
+        if(chechBoxLOL.isChecked()){
+            iIdJuego = 1;
+        }
+        String sUrl = Utils.hosting + "equipo-juego/ins-equipoJuego.php?txtEquipo="+oEquipo.getiIdEquipo.toString()+"&txtFoto="+oProducto.getsFoto();
+
+        Volley.newRequestQueue(this).add(new StringRequest(Request.Method.GET,sUrl,
+                s ->{
+                    if(s.equals("null")){
+                        Toast.makeText(getApplicationContext(), "error al crear el equipo", Toast.LENGTH_LONG).show();
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Equipo creado con éxito", Toast.LENGTH_LONG).show();
+                        insertEquipoJuego();
+                    }
+                }
+                ,volleyError -> {
+
+            Log.d("ALACID",volleyError.getCause().toString());
+        }
+        ));
+
+    }
+
 }
