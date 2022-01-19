ea4cb39e8f7ffbd6455c1a1abac0f8d3e4c7a8a0
Roberto <roberto.munoz.rodriguez99@gmail.com>
2022-01-19 09:54:22 +0100

    Modificado el modelo de Equipo y adaptador.

--- app/src/main/java/com/example/wellplayed/EquipoDetalle.java
+++ app/src/main/java/com/example/wellplayed/EquipoDetalle.java
@@ -18,11 +18,11 @@ public class EquipoDetalle extends AppCompatActivity {
         TextView lblDerrotasDetalle = findViewById(R.id.lblDerrotasEquipo);
         TextView lblWinRateDetalle = findViewById(R.id.lblWinRateEquipo);
 
-        imgViewEquipoDetalle.setImageResource(ListadoEquipos.lstEquipos.get(ListadoEquipos.iEquipoSelected).getoEquipo().getiFoto());
+       /* imgViewEquipoDetalle.setImageResource(ListadoEquipos.lstEquipos.get(ListadoEquipos.iEquipoSelected).getoEquipo().getiFoto());
         lblNombreDetalle.setText(""+ ListadoEquipos.lstEquipos.get(ListadoEquipos.iEquipoSelected).getoEquipo().getsNombre());
         lblVictoriasDetalle.setText(""+ ListadoEquipos.lstEquipos.get(ListadoEquipos.iEquipoSelected).getoEquipo().getsNombre());
         lblDerrotasDetalle.setText(""+ ListadoEquipos.lstEquipos.get(ListadoEquipos.iEquipoSelected).getoEquipo().getsNombre());
         lblWinRateDetalle.setText(""+ ListadoEquipos.lstEquipos.get(ListadoEquipos.iEquipoSelected).getoEquipo().getsNombre());
-
+*/
     }
 }
