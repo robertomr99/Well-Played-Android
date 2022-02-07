13380d12eb6c75eb64b466b3f0a3235def3f3406
Roberto <roberto.munoz.rodriguez99@gmail.com>
2022-02-08 00:41:48 +0100

    Continuando con la funcionalidad de ¨Mis Partidos¨

--- app/src/main/java/com/example/wellplayed/UnirsePartidosAdapter.java
+++ app/src/main/java/com/example/wellplayed/UnirsePartidosAdapter.java
@@ -7,14 +7,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.wellplayed.model.Equipo_Usuario;
 import com.example.wellplayed.model.Partido_Equipo;
 import com.example.wellplayed.model.Partido_Usuario;
 
@@ -29,7 +32,8 @@ public class UnirsePartidosAdapter extends RecyclerView.Adapter<UnirsePartidosAd
 
 
     public interface UnirsePartidosAdapterInterface {
-        void prueba();
+        void unirseAlPartidoEquipo(Partido_Equipo oPartidoEquipo);
+        void unirseAlPartidoUsuario(Partido_Usuario oPartidoUsuario);
     }
 
     public UnirsePartidosAdapter(Context context, UnirsePartidosAdapterInterface upInterface, int iTipo) {
@@ -61,28 +65,46 @@ public class UnirsePartidosAdapter extends RecyclerView.Adapter<UnirsePartidosAd
 
     public void onBindViewHolder(@NonNull UnirsePartidosAdapter.ViewHolder holder, int position) {
 
-        if(iTipoGlobal == 1){
+        if (iTipoGlobal == 1) {
 
             // EQUIPO
 
             Partido_Equipo oPartidoEquipo = ListadoPartidosEquipo.lstPartidoEquipo.get(position); // Instanciamos el objeto de la lista con
 
+            Log.d("ASDASDASDASDASDASDASDASDASD", oPartidoEquipo.toString());
+
             holder.lblNombreC1.setText(oPartidoEquipo.getsNombreEquipo1());
             holder.lblNombreC2.setText(oPartidoEquipo.getsNombreEquipo2());
-            Glide.with(context).load(oPartidoEquipo.getsFotoEquipo1()).error(R.drawable.profile).into(holder.imageViewC1);
-            Glide.with(context).load(oPartidoEquipo.getsFotoEquipo2()).error(R.drawable.profile).into(holder.imageViewC2);
+            Glide.with(context).load(oPartidoEquipo.getsFotoEquipo1()).into(holder.imageViewC1);
+            Glide.with(context).load(oPartidoEquipo.getsFotoEquipo2()).into(holder.imageViewC2);
 
-            if(oPartidoEquipo.getiIdJuego() == 1){
+            holder.imageViewC1.setOnClickListener(view -> {
+                if (oPartidoEquipo.getsFotoEquipo1() == null) {
+                    upInterface.unirseAlPartidoEquipo(oPartidoEquipo);
+                } else {
+                    Toast.makeText(context, "Ya hay un equipo apuntado a este partido", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            holder.imageViewC2.setOnClickListener(view -> {
+                if (oPartidoEquipo.getsFotoEquipo1().isEmpty()) {
+                    Toast.makeText(context, "Únete como primer equipo", Toast.LENGTH_SHORT).show();
+                } else {
+                    upInterface.unirseAlPartidoEquipo(oPartidoEquipo);
+                }
+            });
+
+            if (oPartidoEquipo.getiIdJuego() == 1) {
                 Glide.with(context).load(R.drawable.cvbannerlol).into(holder.imageViewFondo);
-            }else if(oPartidoEquipo.getiIdJuego() == 2){
+            } else if (oPartidoEquipo.getiIdJuego() == 2) {
                 Glide.with(context).load(R.drawable.cvbannevalorant).into(holder.imageViewFondo);
-            }else if(oPartidoEquipo.getiIdJuego() == 3){
+            } else if (oPartidoEquipo.getiIdJuego() == 3) {
                 Glide.with(context).load(R.drawable.cvbannercsgo).into(holder.imageViewFondo);
-            }else{
+            } else {
                 Glide.with(context).load(R.drawable.cvbannefifa).into(holder.imageViewFondo);
             }
 
-        }else{
+        } else {
 
             // USUARIO
 
@@ -90,41 +112,55 @@ public class UnirsePartidosAdapter extends RecyclerView.Adapter<UnirsePartidosAd
 
             holder.lblNombreC1.setText(oPartidoUsuario.getsNombreJugador1());
             holder.lblNombreC2.setText(oPartidoUsuario.getsNombreJugador2());
-            Glide.with(context).load(oPartidoUsuario.getsFotoJugador1()).error(R.drawable.profile).into(holder.imageViewC1);
-            Glide.with(context).load(oPartidoUsuario.getsFotoJugador2()).error(R.drawable.profile).into(holder.imageViewC2);
+            Glide.with(context).load(oPartidoUsuario.getsFotoJugador1()).into(holder.imageViewC1);
+            Glide.with(context).load(oPartidoUsuario.getsFotoJugador2()).into(holder.imageViewC2);
 
-            if(oPartidoUsuario.getiIdJuego() == 1){
+            holder.imageViewC1.setOnClickListener(view -> {
+                if (oPartidoUsuario.getsFotoJugador1() == null) {
+                    upInterface.unirseAlPartidoUsuario(oPartidoUsuario);
+                } else {
+                    Toast.makeText(context, "Ya hay un usuario apuntado como primer jugador", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            holder.imageViewC2.setOnClickListener(view -> {
+                if (oPartidoUsuario.getsFotoJugador1().isEmpty()) {
+                    Toast.makeText(context, "Únete como primer jugador", Toast.LENGTH_SHORT).show();
+                } else {
+                    upInterface.unirseAlPartidoUsuario(oPartidoUsuario);
+                }
+            });
+
+
+            if (oPartidoUsuario.getiIdJuego() == 1) {
                 Glide.with(context).load(R.drawable.cvbannerlol).into(holder.imageViewFondo);
-            }else if(oPartidoUsuario.getiIdJuego() == 2){
+            } else if (oPartidoUsuario.getiIdJuego() == 2) {
                 Glide.with(context).load(R.drawable.cvbannevalorant).into(holder.imageViewFondo);
-            }else if(oPartidoUsuario.getiIdJuego() == 3){
+            } else if (oPartidoUsuario.getiIdJuego() == 3) {
                 Glide.with(context).load(R.drawable.cvbannercsgo).into(holder.imageViewFondo);
-            }else{
+            } else {
                 Glide.with(context).load(R.drawable.cvbannefifa).into(holder.imageViewFondo);
             }
 
         }
 
-
-
         holder.cv.setAnimation(AnimationUtils.loadAnimation(context, R.anim.fade_transition));
-
     }
 
 
     public int getItemCount() {
         int iSize;
-        if(iTipoGlobal == 1){
+        if (iTipoGlobal == 1) {
             iSize = ListadoPartidosEquipo.lstPartidoEquipo.size();
-        }else{
+        } else {
             iSize = ListadoPartidosUsuario.lstPartidoUsuario.size();
         }
         return iSize;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView lblNombreC1,lblNombreC2;
-        ImageView imageViewC1,imageViewC2, imageViewFondo ;
+        TextView lblNombreC1, lblNombreC2;
+        ImageView imageViewC1, imageViewC2, imageViewFondo;
         CardView cv;
 
 
