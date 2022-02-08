b8d4c359da3e6bc43eb69a8b61d0f489902a2641
Miguel <alacidmiguel@gmail.com>
2022-02-08 13:28:12 +0100

    08/02/22

---
+++ app/src/main/java/com/example/wellplayed/PartidoDetalle.java
@@ -0,0 +1,15 @@
+package com.example.wellplayed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PartidoDetalle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partido_detalle);
+    }
+
+}
